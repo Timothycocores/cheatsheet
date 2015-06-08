d51b9f1edd6eecfc8929804a4226ec209969c39f
Timothy Coco <TvRoom@timothys-Mac-mini-2.local>
2015-06-07 23:43:02 -0400

    first commit

---
+++ myscripts.js
@@ -0,0 +1,11 @@
+<!-- Alert name -->
+function sayHelloToThisPerson(name){
+alert('hello : ' + name);
+}
+//run above js function when this js is loaded
+sayHelloToThisPerson('timothy');
+//end alert
+//alert when clicked
+$("p").click(function(){
+        alert("The paragraph was clicked.");
+    });
